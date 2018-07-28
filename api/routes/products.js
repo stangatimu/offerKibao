4fd81ff966748ecbcfceb6670c99e76b519accfe
Stanley Gatimu <stangatimu@gmail.com>
2018-07-28 18:23:20 +0300

    nothing new

--- api/routes/products.js
+++ api/routes/products.js
@@ -8,29 +8,6 @@ const express = require('express'),
  likesController = require('../controllers/likes.js');
  
 
- const storage = multer.diskStorage({
- 	destination: function(req, file, cb){
- 		cb(null,'./uploads');
- 	},
- 	filename: function(req, file, cb){
- 		cb(null, new Date().toDateString() + file.originalname);
- 	}
- });
-
-const fileFilter = (req,file, cb)=>{
-	if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-		cb(null, true);
-	} else {
-		cb(null, false);
-	}
-}
-const upload = multer({storage: storage,
-	                   limits:{
-	                   	fileSize: 1024 * 1024 * 5
-	                   },
-	                   fileFilter: fileFilter
-                     });
-
 router.get('/',rateLimit.globalBF.prevent, productsController.products_get_all );
 
 router.get('/location',rateLimit.globalBF.prevent,productsController.products_get_by_location);
